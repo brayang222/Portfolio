bbe9f5c51dd308f159504a31ebbcabd7925fa5b8
brayang222 <116692605+brayang222@users.noreply.github.com>
2023-07-04 17:22:52 -0500

    portfolio

---
+++ README.md
@@ -0,0 +1,40 @@
+<!-- Please update value in the {}  -->
+
+<h1 align="center">{This is my personal portfolio}</h1>
+
+
+<div align="center">
+  <h3>
+    <a href="https://portfoliobra.netlify.app/">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://github.com/brayang222/Portfolio">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/wBunSb7FPrIepJZAg0sY">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- OVERVIEW -->
+
+## Overview
+
+![screenshot](/screen.png)
+
+### Built With
+
+
+- [HTML](https://developer.mozilla.org/es/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/es/docs/Web/CSS)
+- [JavaScript](https://developer.mozilla.org/es/docs/Web/JavaScript)
+
+
+## Contact
+
+- Website [portfolio.com](https://portfoliobra.netlify.app/)
+- GitHub [@brayang222](https://github.com/your-usermame)
+- Twitter [@your-twitter](https://twitter.com/BrayanGmz_)
---
+++ app.js
@@ -0,0 +1,86 @@
+const TituloProyect = document.getElementsByClassName('text-proyect')[0];
+const h1Titulo = document.createElement('h1');
+h1Titulo.textContent = "<Proyects />";
+
+TituloProyect.appendChild(h1Titulo);
+// ------------------------ MOSTRAR MÁS -------------------------------------
+const hidden = document.getElementsByClassName('proyects-container');
+const btnMostrarImg = document.getElementById('mostrar-img');
+
+btnMostrarImg.addEventListener('click', () => {
+  
+  for (let i = 0; i < hidden.length; i++) {
+    const elementoPadre = hidden[i];
+    const childrenList = elementoPadre.children;
+  
+    for (let j = 2; j < childrenList.length; j++) {
+      const nodo = childrenList[j];
+      nodo.classList.toggle('hidden');
+    }
+  }
+  const i = document.getElementById('mostrar-i')
+  if (btnMostrarImg.textContent === 'Menos') {
+    btnMostrarImg.textContent = 'Ver más'
+    i.classList.add('fa-solid', 'fa-caret-down')
+    btnMostrarImg.append(i)
+    
+  } else {
+    btnMostrarImg.textContent = 'Menos';
+    i.classList.remove('fa-caret-down')
+    i.classList.add('fa-caret-up')
+    btnMostrarImg.append(i)
+  }
+
+})
+
+//-------------------------- FIN MOSTRAR MÁS -----------------------------------------
+
+const btnSwitch = document.querySelector('#switch');
+const languaje = document.getElementById('languaje');
+
+btnSwitch.addEventListener('click', () => {
+  
+  btnSwitch.classList.toggle('active');
+
+  if (btnSwitch.classList.contains('active')) {
+    languaje.textContent = `I am a fast learner, always willing to learn new skills, 
+  keep up to date with the latest technologies and programming practices, with a
+   strong background in web programming, with basic knowledge of php, c#, 
+   javascript and excel visual basic (creating macros), Data management with MySQL
+    and a passion for solving complex problems as well. My ability to analyze,
+     design and develop efficiently allows me to deliver exceptional results.`
+  } else {
+    languaje.textContent = `Soy un aprendiz rápido, siempre dispuesto a
+    adquirir nuevas habilidades, mantenerme
+    actualizado con las últimas tecnologías y
+    prácticas de programación, con una sólida
+    formación en programación web, con
+    conocimientos básicos de php, c#, JavaScript
+    y visual basic de excel (creando macros),
+    también manejo de datos con MySQL y una
+    pasión por resolver problemas complejos. Mi
+    capacidad para analizar, diseñar y
+    desarrollar eficientemente me permite entregar resultados excepcionales.`
+  };
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
